66b1032a5b497f889493f1257092816224a22ef5
anju <codebyanju@gmail.com>
2025-09-11 00:27:35 +0530

    Outlet, Link - Router

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
       <Footer />
     </div>
   );
@@ -23,15 +23,21 @@ const appRouter = createBrowserRouter([
     path: "/",
     element: <AppLayout />,
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  ,
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      ,
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import { LOGO_URL } from "../utils/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  // Never write useState Hook outside a React component or a custom Hook
+  // const [btnName, setBtnName] = useState("Login");
+  // const Header = () => {
+  //   ...
+  // }
+  // Never write useState hooks inside conditions or loops or functions
+  // This breaks the Rules of Hooks and creates inconsistencies in the order of Hook calls during re-renders.
+  // if(true){
+  //   const [btnName, setBtnName] = useState("Login");
+  // }
+  // State variables are meant to be created inside your functional component so that each instance of the component maintains its own state.
+  // If you define a state variable outside the component, it will be shared across all instances of that component, leading to unexpected behavior. Its not a good practice
+  // Always keep them on the top level of the component.
   const [btnName, setBtnName] = useState("Login");
+
+  // If no dependency array is provided => the useEffect runs AFTER every render of the component.
+  // If dependency array is empty => the useEffect runs only once after the initial render. (Only Once)
+  // If dependency array has variables => the useEffect runs after the initial render and whenever any of the dependencies change.
+  useEffect(() => {
+    console.log("useEffect called");
+  });
 
   return (
     <div className="header">
@@ -11,9 +32,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home </Link>
+          </li>
+          <li>
+            <Link to="/about">About Us </Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us </Link>
+          </li>
           <li>Cart</li>
           <li
             className="login"
