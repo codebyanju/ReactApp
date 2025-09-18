e27862e5021b2405c49b1669cc4b544076621b2d
anju <codebyanju@gmail.com>
2025-09-19 01:16:11 +0530

    image

--- src/components/CategoryItemsAccordion.js
+++ src/components/CategoryItemsAccordion.js
@@ -1,19 +1,29 @@
+import { CDN_URL } from "../utils/constants";
 const CategoryItemsAccordion = ({ catItems }) => {
   //   console.log("catitems", catItems);
 
   return (
     <div>
       {catItems.map((item) => {
-        console.log(item);
-        const { id, name, price, description } = item.card.info;
-        return (
-          <div className="flex flex-col my-2 py-1 gap-2" key={id}>
-            <span className="text-md font-semibold ">
-              {name} - ₹ {price / 100}
-            </span>
+        const { id, name, price, description, imageId } = item.card.info;
 
-            <span className="text-sm text-gray-600">{description}</span>
-            <hr className="border-gray-300 mt-2" />
+        console.log(item);
+        return (
+          <div key={id}>
+            <div className="flex justify-between my-2 py-1 gap-2">
+              <div className="flex flex-col">
+                <span className="text-md font-semibold">
+                  {name} - ₹ {price / 100}
+                </span>
+                <span className="text-sm text-gray-600">{description}</span>
+              </div>
+
+              <div className="flex items-center max-w-[100px]">
+                <img src={CDN_URL + imageId} alt={name} />
+              </div>
+            </div>
+
+            <hr className="border-t border-gray-300" />
           </div>
         );
       })}
